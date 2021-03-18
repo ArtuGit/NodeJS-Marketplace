6ddc3d38751c3a29aa95613c7cef51279354dc97
Artem Laptiev <artuon@gmail.com>
2021-03-18 11:40:38 +0200

    Set app port

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
