f7ea0213becab9fbde34ee771f5bd697d31709c6
Artem L <artuon@gmail.com>
2021-03-20 21:34:42 +0200

    Check email better

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 - Adding H1 header for all pages
 - Adding and testing of the working 3rd-party credentials: Stripe, SendGrid
 - Adding price and sum to the cart and checkout pages
-- Adding confirmatory messages for auth operations: sign up, password reset,new password   
+- Adding confirmatory messages for auth operations: sign up, password reset,new password
+- Adding User Edit Page with all checking and possibility of the password changing
--- routes/auth.js
+++ routes/auth.js
@@ -31,31 +31,31 @@ router.post(
   [
     check('email')
       .isEmail()
+      .normalizeEmail()
       .withMessage('Please enter a valid email.')
       .custom((value, { req }) => {
         // if (value === 'test@test.com') {
         //   throw new Error('This email address if forbidden.');
         // }
         // return true;
-        return User.findOne({ email: value }).then(userDoc => {
+        return User.findOne({email: value}).then(userDoc => {
           if (userDoc) {
             return Promise.reject(
               'E-Mail exists already, please pick a different one.'
             );
           }
         });
-      })
-      .normalizeEmail(),
+      }),
     body(
       'password',
       'Please enter a password with only numbers and text and at least 5 characters.'
     )
-      .isLength({ min: 5 })
+      .isLength({min: 5})
       .isAlphanumeric()
       .trim(),
     body('confirmPassword')
       .trim()
-      .custom((value, { req }) => {
+      .custom((value, {req}) => {
         if (value !== req.body.password) {
           throw new Error('Passwords have to match!');
         }
