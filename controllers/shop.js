c0ec5c4f00e491631cfc3da1986593883ea8dcea
Artem Laptiev <artuon@gmail.com>
2021-03-24 22:58:55 +0200

    Adding products

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,7 +10,7 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITEMS_PER_PAGE = 4;
+const ITEMS_PER_PAGE = 6;
 
 exports.getProducts = (req, res, next) => {
   const page = +req.query.page || 1;
